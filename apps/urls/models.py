df7c65ee00faa8ff1150a051cdfa6a4d58f4cff9
Kol <kol478@gmail.com>
2023-03-05 16:21:57 +0100

    Add comments

--- apps/urls/models.py
+++ apps/urls/models.py
@@ -8,5 +8,7 @@ class ShortUrl(models.Model):
     short_url = models.CharField(max_length=15, unique=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # as a feature, we can also add an expiration date here
+
     def __str__(self):
         return f"{self.original_url} to {self.short_url}"
--- apps/urls/utils/generate_code.py
+++ apps/urls/utils/generate_code.py
@@ -3,7 +3,7 @@ import hashlib
 import base62
 
 
-def generate_code(url: str, size: int) -> str:
+def generate_code(url: str, *, size: int) -> str:
     md5 = hashlib.md5(url.encode()).digest()
     base62_encoded = base62.encodebytes(md5)
     return base62_encoded[:size]
